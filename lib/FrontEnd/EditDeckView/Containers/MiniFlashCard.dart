b8baa4720010eddad7a9457e11a98987595db8c0
DESKTOP-U26BJ62\T-Gamer <vpv.9@hotmail.com>
2021-06-10 23:44:47 -0300

    tentativa de usar fractionallysizedbox

--- lib/FrontEnd/EditDeckView/Containers/MiniFlashCard.dart
+++ lib/FrontEnd/EditDeckView/Containers/MiniFlashCard.dart
@@ -2,12 +2,49 @@ import 'package:flutter/material.dart';
 
 class MiniFlashCard  extends StatelessWidget {
   MiniFlashCard({Key? key}) : super(key: key);
-  @override
+  String teste= 'aa';
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: Color(0xff57C4E5),
-      margin: const EdgeInsets.all(10.0),
-    );
+    return Container(
+          alignment: Alignment.center,
+          margin: EdgeInsets.all(20),
+          padding: EdgeInsets.all(30),
+          decoration: BoxDecoration(
+            color: Color.fromRGBO(87, 196, 229, 1),
+            borderRadius: BorderRadius.all(Radius.circular(20)),
+          ),
+            child:Row ( 
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children:[
+                FractionallySizedBox(
+                  widthFactor: 0.25,
+                  heightFactor: 0.25,
+                  alignment: Alignment.bottomRight,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    
+          ), ),),
+          SizedBox(width:50),
+          FractionallySizedBox(
+                  widthFactor: 0.25,
+                  heightFactor: 0.25,
+                  alignment: Alignment.bottomRight,
+                child: 
+          Container(
+            
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+          ),
+
+                ),)
+            ]
+          )
+          
+);
+    
   }
 }
--- lib/FrontEnd/EditDeckView/EditDeckView.dart
+++ lib/FrontEnd/EditDeckView/EditDeckView.dart
@@ -4,68 +4,72 @@ import 'package:flutter/material.dart';
 import 'Containers/BottomEditBar.dart';
 
 class DeckEdit extends StatelessWidget {
-  DeckEdit({Key? key}) : super (key: key);
+  DeckEdit({Key? key}) : super(key: key);
+  String joao="joao";
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           backgroundColor: Color.fromRGBO(87, 196, 229, 1),
+          title: RichText(
+            text: TextSpan(
+              children: [
+                TextSpan(
+                  text: joao,
+                  style: TextStyle(
+                    color: Color(0xff8FDC97),
+                    fontSize: 30,
+                  ),
+                ),
+              ],
+            ),
+          ),
           actions: [
             IconButton(
               icon: Icon(Icons.edit),
               color: Colors.white,
-            onPressed: (){
-              Navigator.of(context).pushNamed('/EditDeckView');
-            },
+              onPressed: () {
+                Navigator.of(context).pushNamed('/CreateDeckView');
+              },
+            ),
+          ],
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back_ios_new_sharp),
+            color: Colors.white,
+            onPressed: () => {Navigator.of(context).pop()},
           ),
-        ],
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back_ios_new_sharp),
-          color: Colors.white,
-          onPressed: () => {Navigator.of(context).pop()},
         ),
-      ),
-      body: CardCreation(),
+        body: CardCreation(),
         floatingActionButton: FloatingActionButton(
-          onPressed: (){
-
-            Navigator.of(context).pushNamed('/CreateDeckView');
+          onPressed: () {//adicionar algo na lista para receber o card//
           },
-          tooltip: 'New Deck',
+          tooltip: 'New Card',
           child: Icon(Icons.add),
           backgroundColor: Color(0xff8FDC97),
           elevation: 10.0,
         ),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-        bottomNavigationBar: BottomEditBar(key: key)
-    );
+        bottomNavigationBar: BottomEditBar(key: key));
   }
 }
 
 class CardCreation extends StatelessWidget {
-
-
-
-
-
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget> [
-        Expanded(
-          child:GridView.builder(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-            itemCount: 10 ,
-            itemBuilder: (BuildContext context,int index) {
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Expanded(
+              child: GridView.builder(
+            gridDelegate:
+                SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 1),
+                
+            itemCount: 2,
+            itemBuilder: (BuildContext context, int index) {
               return MiniFlashCard();
-             },
-             ) 
-        )
-
-      ]
-    );
-               
+            },
+          )
+          )
+        ]);
   }
 }
-
