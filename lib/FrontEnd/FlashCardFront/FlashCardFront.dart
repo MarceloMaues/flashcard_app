927d549bc275c37eae6e1010a627722044ab77cf
HE-SERIES-PC\HE-SERIES <marcelinhomaues@hotmail.com>
2021-06-10 21:49:53 -0300

    Botei animação e os botões de certo e errado, falta fazer a rota quando o usario apertar o botão

--- lib/FrontEnd/FlashCardFront/FlashCardFront.dart
+++ lib/FrontEnd/FlashCardFront/FlashCardFront.dart
@@ -1,33 +1,36 @@
 import 'package:flutter/material.dart';
-
+import 'package:flip_card/flip_card.dart';
 
 class GameView extends StatelessWidget {
   GameView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromRGBO(87, 196, 229, 1),
+      appBar: AppBar(
         backgroundColor: Color.fromRGBO(87, 196, 229, 1),
-        appBar: AppBar(
-          backgroundColor: Color.fromRGBO(87, 196, 229, 1),
-          actions: [
-            IconButton(
-              icon: Icon(Icons.edit),
-              color: Colors.white,
-              onPressed: (){
-                Navigator.of(context).pushNamed('/EditDeckView');
-              },
-            ),
-          ],
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back_ios_new_sharp),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.edit),
             color: Colors.white,
-            onPressed: () => {Navigator.of(context).pop()},
+            onPressed: () {
+              Navigator.of(context).pushNamed('/EditDeckView');
+            },
           ),
+        ],
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios_new_sharp),
+          color: Colors.white,
+          onPressed: () => {Navigator.of(context).pop()},
         ),
-        body: Center(child: FlashcardFront()),
-      );
+      ),
+      body: Center(child: FlashcardFront()),
+    );
   }
 }
+
+int counterIsCorrect = 0;
+int counterIsWrong = 0;
 
 class FlashcardFront extends StatelessWidget {
   @override
@@ -60,44 +63,83 @@ class FlashcardFront extends StatelessWidget {
                 ],
               ),
             ]),
-        Container(
-          child: Text(
-            'Texto aaaaaa',
-            style: TextStyle(
-              fontSize: 20.0,
-              color: Colors.black,
-            ),
-          ),
-          alignment: Alignment.center,
-          width: 200,
-          height: 200,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(20)),
-            color: Color.fromRGBO(255, 255, 255, 1),
-          ),
-        ),
-        Row(
-          children: <Widget>[
-            SizedBox(height: 100),
-          ],
-        ),
-        Row(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                primary: Color.fromRGBO(143, 220, 151, 1),
-                elevation: 20.0,
-                textStyle: TextStyle(
-                  fontSize: 30,
+        FlipCard(
+            direction: FlipDirection.HORIZONTAL,
+            front: Column(
+              children: <Widget>[
+                Container(
+                  child: Text(
+                    'Texto aaaaaa',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.black,
+                    ),
+                  ),
+                  alignment: Alignment.center,
+                  width: 200,
+                  height: 200,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    color: Color.fromRGBO(255, 255, 255, 1),
+                  ),
                 ),
-              ),
-              child: const Text('  Turn  ', ),
+                Row(
+                  children: <Widget>[
+                    SizedBox(height: 78),
+                  ],
+                ),
+              ],
             ),
-          ],
-        ),
+            back: Column(
+              children: <Widget>[
+                Container(
+                  child: Text(
+                    'Texto costaaaaa',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.black,
+                    ),
+                  ),
+                  alignment: Alignment.center,
+                  width: 200,
+                  height: 200,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    color: Color.fromRGBO(255, 255, 255, 1),
+                  ),
+                ),
+                Row(
+                  children: <Widget>[
+                    SizedBox(height: 50),
+                  ],
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      child: ElevatedButton(
+                        child: Text("Yes Yes Yes Yes Yes"),
+                        onPressed: () {
+                          counterIsCorrect++;
+                          print(counterIsCorrect);
+                        },
+                      ),
+                    ),
+                    SizedBox(width: 50),
+                    Container(
+                      child: ElevatedButton(
+                        child: Text("No No No No No No"),
+                        onPressed: () {
+                          counterIsWrong++;
+                          print(counterIsWrong);
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            )),
       ],
     );
   }
